2ee03ee7b57f416c5e34434bbbfb8ea48e475e70
CaraV89 <carolinev429@gmail.com>
2017-03-03 18:09:35 -0800

    Updates
    Updated Background
    Edited Google Doodle “E”
    Animated Google Doodle using Animate.css
    Added script for randomization

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,10 @@
 // YOUR SCRIPTS GO HERE
 
-//Store Image URLs
-var img0 = '../img/200.jpg';
-var img1 = '../img/300.jpg';
-var img2 = '../img/400.jpg';
+// Random Number Generator
+var limit = 3;
+var randNum = Math.floor(Math.random() * limit);
+console.log(randNum);
 
-// Get Random Number
-var max = 3;
-var numRand = Math.floor(Math.random() * max);
-console.log(numRand);
-
-//Show Images
-//var tmp = $('img').attr( 'src', eval('img' + numRand));
-//console.log(tmp);
+// Get Random Body Class
+// Change Background Color
+$('body').addClass('bg' + randNum );
